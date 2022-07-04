074ba5f4c9186c0dd82c561dfb4ba999738e4363
snehapal2012 <snehapal2012@gmail.com>
2022-07-04 18:50:39 +0530

    Added:Insertion sort using string

---
+++ src/com/algorithmprograms/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.algorithmprograms;
+
+public class InsertionSort {
+    public static void main(String[] args){
+        String[] arr ={"Kolkata","New York","Mumbai","Delhi","Hyderabad"};
+        int count = 0;
+        String sortedArray[] = sort_sub(arr, arr.length);
+        for(int i=0;i<sortedArray.length;i++){
+            System.out.println(sortedArray[i]);
+        }
+    }
+
+    public static String[] sort_sub(String array[], int size){
+        String temp="";
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(array[i].compareToIgnoreCase(array[j])>0){
+                    temp = array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        return array;
+    }
+}
